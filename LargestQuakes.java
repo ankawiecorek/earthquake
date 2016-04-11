f31977ff0702a90537ae2d0bf11efb4d4fc09ef5
ankawiecorek <anna.wieczorek906@gmail.com>
2016-04-12 00:01:14 +0200

    Method findLargestQuakes added. Reading data and storing them.

--- LargestQuakes.java
+++ LargestQuakes.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class LargestQuakes {
 	
-	
+	public void findLargestQuakes() {
+		EarthQuakeParser parser = new EarthQuakeParser();
+		String source = "data/nov20quakedatasmall.atom";
+		
+		ArrayList<QuakeEntry> list  = parser.read(source);
+		System.out.println("read data for " + list.size());
+	}
 
 }
